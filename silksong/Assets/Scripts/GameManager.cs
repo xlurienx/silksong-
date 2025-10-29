dd666d6152e744a213dd98a5a74fab63114d4014
llghostinll <158472155+llghostinll@users.noreply.github.com>
2025-10-29 14:51:29 -0700

    V0.0.8
    -added timer
    -fix item collision
    - spawner timer

--- silksong/Assets/Scripts/GameManager.cs
+++ silksong/Assets/Scripts/GameManager.cs
@@ -3,17 +3,37 @@ using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
+    private float timer;
+    public float levelDuration = 60;
+    public bool isLevelFinished;
+    public string nextLevelName;
     public Health playerHealth;
     public Health shoreHealth;
-    private void Awake()
+    private void Awake() // when the game starts
     {
         playerHealth.OnDeath += Lose;
         shoreHealth.OnDeath += Lose;
     }
-    private void OnDestroy()
+    private void OnDestroy() // Restart
     {
         playerHealth.OnDeath -= Lose;
         shoreHealth.OnDeath -= Lose;
+    }
+    private void Update()
+    {
+        timer += Time.deltaTime;
+        if (timer > levelDuration)
+        {
+            if (!isLevelFinished)
+            {
+                isLevelFinished = true;
+                FinishLevel();
+            }
+        }
+    }
+    private void FinishLevel ()
+    {
+        SceneManager.LoadScene(nextLevelName);
     }
     public void Lose()
     {
